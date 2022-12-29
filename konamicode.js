d5b5618c68169f40d3d1f7bc39764cb93aa2f111
Alex Vasse <alexandervasse@gmail.com>
2022-12-30 01:51:08 +0900

    prevented sound from looping

--- konamicode.js
+++ konamicode.js
@@ -1,11 +1,30 @@
 const pressed = [];
 const secretCode = 'ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightbaEnter';
+let correct = true;
 
-  window.addEventListener('keyup', (event) => {
-    pressed.push(event.key);
-    pressed.splice(-secretCode.length -1, pressed.length - secretCode.length)
-    if (pressed.join('').includes(secretCode)) {
-      console.log ("DING DING");
-    }
-    console.log(pressed.join(''))
-  })
+window.addEventListener('keydown', (event) => {
+  pressed.push(event.key);
+  pressed.splice(-secretCode.length -1, pressed.length - secretCode.length)
+  if (pressed.join('').includes(secretCode) && correct) {
+    let jingle = new Audio('konami.wav');
+    console.log ("DING DING");
+    jingle.play();
+    correct = false;
+  }
+  console.log(pressed.join(''))
+});
+
+function removeTransition(event) {
+  if (event.propertyName !== 'transform') return;
+  this.classList.remove('highlight');
+}
+
+function highlight(event) {
+  const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+  key.classList.add('highlight');
+};
+
+window.addEventListener('keydown', highlight);
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
